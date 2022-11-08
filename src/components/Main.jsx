176e0e1d046b73674f7246ba9f1a8bce07a691ae
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2022-11-08 13:01:16 +0600

    fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,11 +9,36 @@ const Main = () => {
 
   const [active, setActive] = useState(0);
 
+  const takeProfit = (item, flap) => {
+    let result = (item * 100) / flap;
+    if (modeFirst === 2) {
+      if (result < 100) {
+        result = result - 100;
+      }
+    } else {
+      if (result > 100) {
+        result = 100 - result;
+      }
+    }
+
+    return (
+      <div
+        className={
+          result < 0
+            ? "text-14  bg-red px-[16px] pu-[12px] m-auto text-white rounded-6"
+            : "text-14 bg-green px-[16px] pu-[12px] m-auto text-white rounded-6"
+        }
+      >
+        {result.toFixed(3)}%
+      </div>
+    );
+  };
+
   return (
     <div className="mt-[124px] w-[100vw] bg-secondary">
       <div className="2xl:w-[1290px] mx-auto rounded-20 h-max bg-main px-[36px] py-[24px]">
         <div className="grid bg-secondary w-full h-max rounded-10 px-[24px] py-[12px]">
-          {modeFirst === 2 && modeSecond === 1 ? (
+          {modeFirst === 2 ? (
             <>
               {ordersArrayFirst !== null ? (
                 <>
@@ -232,9 +257,249 @@ const Main = () => {
                                   {flap.type === 2 ? "Buy" : "Sell"}
                                 </p>
 
-                                <div className="text-14 border border-1 border-red px-[12px] pu-[8px] m-auto text-white rounded-6">
-                                  {(item.price - flap.price).toFixed(3)} %
+                                {takeProfit(item.price, flap.price)}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      ) : null}
+                    </div>
+                  ))}
+                </>
+              ) : null}
+            </>
+          ) : modeFirst === 1 ? (
+            <>
+              {ordersArrayFirst !== null ? (
+                <>
+                  {ordersArrayFirst.map((item) => (
+                    <div
+                      className={
+                        item.id !== active
+                          ? "bg-main rounded-10 h-max min-h-[100px] mb-[24px] grid"
+                          : "bg-main rounded-10 h-max min-h-[100px] mb-[24px] grid border border-1 border-light-blue"
+                      }
+                    >
+                      <div className="flex min-h-[100px] h-max" key={item.id}>
+                        <div className="grid h-full w-[262px] py-[12px] my-auto">
+                          <p className="w-max h-max mx-auto my-auto text-white mb-[8px] text-18">
+                            {item.trade_user.name}
+                          </p>
+
+                          <div className="w-max flex h-max mx-auto">
+                            <p className="w-max h-max text-light-blue text-14 mr-[4px]">
+                              {item.trade_user.month_orders_count}
+                            </p>
+                            <p className="w-max h-max text-white text-12 my-auto">
+                              orders
+                            </p>
+
+                            <p className="w-max h-max text-light-blue text-14 ml-[12px] mr-[4px]">
+                              {item.trade_user.month_finish_rate * 100}%
+                            </p>
+                            <p className="w-max h-max text-white text-12 my-auto">
+                              completion
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="grid h-full w-[244px] py-[12px] my-auto">
+                          <div className="flex justify-center h-max my-auto">
+                            <p className="w-max h-max my-auto text-light-blue text-18">
+                              {item.price}
+                            </p>
+                            <p className="text-white my-auto text-12 ml-[8px]">
+                              {item.fiat.name}
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="grid h-full w-[268px] py-[12px] my-auto">
+                          <div className="flex my-auto mr-auto">
+                            <p className="text-10 text-white w-max h-max my-auto">
+                              Available
+                            </p>
+                            <p className="text-light-blue text-16 mx-[8px]">
+                              {item.amount}
+                            </p>
+                            <p className="text-white text-12 w-max h-max my-auto">
+                              {item.asset.name}
+                            </p>
+                          </div>
+                          <div className="flex my-auto mr-auto">
+                            <p className="text-10 text-white w-max h-max my-auto">
+                              Limit
+                            </p>
+                            <p className="text-light-blue text-14 mr-[6px] ml-[8px]">
+                              {item.min_trans_amount}
+                            </p>
+                            <p className="text-10 text-white w-max h-max my-auto">
+                              {" "}
+                              -{" "}
+                            </p>
+                            <p className="text-light-blue text-14 mx-[6px]">
+                              {item.max_trans_amount}
+                            </p>
+                            <p className="text-white my-auto text-12">
+                              {item.fiat.name}
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="w-[188px] h-max py-[12px] flex flex-wrap my-auto">
+                          {item.trade_methods.map((obj) => (
+                            <div className="flex w-max h-max" key={obj.id}>
+                              <div
+                                className={
+                                  item.type === 2
+                                    ? "w-max h-max rounded-6 border border-1 border-green px-[4px] py-[2px]"
+                                    : "w-max h-max rounded-6 border border-1 border-red px-[4px] py-[2px]"
+                                }
+                              >
+                                <p className="w-max h-max text-12 text-white">
+                                  {obj.name}
+                                </p>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+
+                        <div className="w-[210px] h-full py-[12px] flex my-auto mx-auto my-auto">
+                          <p
+                            className={
+                              item.type === 2
+                                ? "w-max h-max my-auto mx-auto text-16 border border-1 border-green text-white px-[12px] py-[px] rounded-6"
+                                : "w-max h-max my-auto mx-auto text-16 border border-1 border-red text-white px-[12px] py-[px] rounded-6"
+                            }
+                          >
+                            {item.type === 2 ? "Buy" : "Sell"}
+                          </p>
+
+                          <button
+                            type="button"
+                            onClick={
+                              active !== item.id
+                                ? () => setActive(item.id)
+                                : () => setActive(0)
+                            }
+                            className={
+                              active !== item.id
+                                ? "w-[20px] h-[20px] my-auto mx-auto"
+                                : "w-[20px] h-[20px] my-auto rotate-180 mx-auto "
+                            }
+                          >
+                            <img src={chevron} alt="opt" className="z-2" />
+                          </button>
+                        </div>
+                      </div>
+                      {active === item.id ? (
+                        <div className="bg-main border-t border-t-1 border-t-light-blue h-max min-h-[10px] rounded-b-10">
+                          {ordersArraySecond.map((flap) => (
+                            <div
+                              className="flex h-max w-full bg-main rounded-b-10 border-b border-b-1 border-b-secondary"
+                              key={flap.id}
+                            >
+                              <div className="grid h-full w-[262px] py-[16px] my-auto">
+                                <p className="w-max h-max mx-auto my-auto text-white mb-[8px] text-18">
+                                  {flap.trade_user.name}
+                                </p>
+
+                                <div className="w-max flex h-max mx-auto">
+                                  <p className="w-max h-max text-light-blue text-14 mr-[4px]">
+                                    {flap.trade_user.month_orders_count}
+                                  </p>
+                                  <p className="w-max h-max text-white text-12 my-auto">
+                                    orders
+                                  </p>
+
+                                  <p className="w-max h-max text-light-blue text-14 ml-[12px] mr-[4px]">
+                                    {(
+                                      flap.trade_user.month_finish_rate * 100
+                                    ).toFixed(1)}
+                                    %
+                                  </p>
+                                  <p className="w-max h-max text-white text-12 my-auto">
+                                    completion
+                                  </p>
                                 </div>
+                              </div>
+
+                              <div className="grid h-full w-[244px] py-[12px] my-auto">
+                                <div className="flex justify-center h-max my-auto">
+                                  <p className="w-max h-max my-auto text-light-blue text-18">
+                                    {flap.price}
+                                  </p>
+                                  <p className="text-white my-auto text-12 ml-[8px]">
+                                    {flap.fiat.name}
+                                  </p>
+                                </div>
+                              </div>
+
+                              <div className="grid h-full w-[268px] py-[12px] my-auto">
+                                <div className="flex my-auto mr-auto">
+                                  <p className="text-10 text-white w-max h-max my-auto">
+                                    Available
+                                  </p>
+                                  <p className="text-light-blue text-16 mx-[8px]">
+                                    {flap.amount}
+                                  </p>
+                                  <p className="text-white text-12 w-max h-max my-auto">
+                                    {flap.asset.name}
+                                  </p>
+                                </div>
+                                <div className="flex my-auto mr-auto">
+                                  <p className="text-10 text-white w-max h-max my-auto">
+                                    Limit
+                                  </p>
+                                  <p className="text-light-blue text-14 mr-[6px] ml-[8px]">
+                                    {flap.min_trans_amount}
+                                  </p>
+                                  <p className="text-10 text-white w-max h-max my-auto">
+                                    {" "}
+                                    -{" "}
+                                  </p>
+                                  <p className="text-light-blue text-14 mx-[6px]">
+                                    {flap.max_trans_amount}
+                                  </p>
+                                  <p className="text-white my-auto text-12">
+                                    {flap.fiat.name}
+                                  </p>
+                                </div>
+                              </div>
+
+                              <div className="w-[188px] h-max py-[12px] flex flex-wrap my-auto">
+                                {flap.trade_methods.map((pas) => (
+                                  <div
+                                    className="flex w-max h-max"
+                                    key={pas.id}
+                                  >
+                                    <div
+                                      className={
+                                        flap.item === 2
+                                          ? "w-max h-max rounded-6 border border-1 border-red px-[4px] py-[2px]"
+                                          : "w-max h-max rounded-6 border border-1 border-green px-[4px] py-[2px]"
+                                      }
+                                    >
+                                      <p className="w-max h-max text-12 text-white">
+                                        {pas.name}
+                                      </p>
+                                    </div>
+                                  </div>
+                                ))}
+                              </div>
+
+                              <div className="w-[210px] h-full py-[12px] flex my-auto mx-auto my-auto">
+                                <p
+                                  className={
+                                    flap.type === 2
+                                      ? "w-max h-max my-auto mx-auto text-16 border border-1 border-green text-white px-[12px] py-[px] rounded-6"
+                                      : "w-max h-max my-auto mx-auto text-16 border border-1 border-red text-white px-[12px] py-[px] rounded-6"
+                                  }
+                                >
+                                  {flap.type === 2 ? "Buy" : "Sell"}
+                                </p>
+
+                                {takeProfit(item.price, flap.price)}
                               </div>
                             </div>
                           ))}
