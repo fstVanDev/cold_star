c345bdaabd487fe887310ea2f60031c1fd4cba05
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2022-12-19 14:16:19 +0600

    fix

--- src/components/filterComponents/Refresh.jsx
+++ src/components/filterComponents/Refresh.jsx
@@ -21,68 +21,72 @@ const Refresh = ({
     <button
       className="w-max h-full flex border border-1 border-orange rounded-10 "
       type="button"
-      onClick={async () => {
-        if (
-          currentFiat !== null &&
-          currentCrypto !== null &&
-          currentPayment !== null
-        ) {
-          await getOrders(
-            mode,
-            currentAmount,
-            currentFiat,
-            currentCrypto,
-            currentPayment,
-            setCurrentOrders
-          );
-          const localObject = {
-            id: globalId,
-            mode: mode,
-            amount: currentAmount,
-            defaultAmount: currentAmount.length === 0 ? false : true,
-            fiat: currentFiat,
-            crypto: currentCrypto,
-            payments: currentPayment,
-            orders: currentOrders,
-            secondaryOrders: secondaryOrders === null ? null : secondaryOrders,
-          };
+      onClick={() => {
+        getOrders(
+          mode,
+          currentAmount,
+          currentFiat,
+          currentCrypto,
+          currentPayment,
+          setCurrentOrders
+        );
 
-          if (config === null) {
-            const arr = [];
-            arr.push(localObject);
+        window.setTimeout(function () {
+          if (
+            currentFiat !== null &&
+            currentCrypto !== null &&
+            currentPayment !== null
+          ) {
+            const localObject = {
+              id: globalId,
+              mode: mode,
+              amount: currentAmount,
+              defaultAmount: currentAmount.length === 0 ? false : true,
+              fiat: currentFiat,
+              crypto: currentCrypto,
+              payments: currentPayment,
+              orders: currentOrders,
+              secondaryOrders:
+                secondaryOrders === null ? null : secondaryOrders,
+            };
 
-            setConfig(arr);
+            if (config === null) {
+              const arr = [];
+              arr.push(localObject);
+
+              setConfig(arr);
+            } else {
+              let arr = config;
+
+              arr.map((item, index) => {
+                if (item.id === globalId && arr[arr.length - 1] !== globalId) {
+                  if (JSON.stringify(item) !== JSON.stringify(localObject)) {
+                    arr.splice(index, 1);
+                    const insert = function (array, indexi, obj) {
+                      return [
+                        ...array.slice(0, indexi),
+                        obj,
+                        ...array.slice(indexi),
+                      ];
+                    };
+                    arr = insert(arr, index, localObject);
+                    return setConfig(arr);
+                  }
+                } else {
+                  if (arr.length - 1 !== globalId) {
+                    arr.push(localObject);
+                    return setConfig(arr);
+                  }
+                }
+              });
+            }
+            setNewFilterView(false);
           } else {
-            let arr = config;
-
-            arr.map((item, index) => {
-              if (item.id === globalId && arr[arr.length - 1] !== globalId) {
-                if (JSON.stringify(item) !== JSON.stringify(localObject)) {
-                  arr.splice(index, 1);
-                  const insert = function (array, indexi, obj) {
-                    return [
-                      ...array.slice(0, indexi),
-                      obj,
-                      ...array.slice(indexi),
-                    ];
-                  };
-                  arr = insert(arr, index, localObject);
-                  return setConfig(arr);
-                }
-              } else {
-                if (arr.length - 1 !== globalId) {
-                  arr.push(localObject);
-                  return setConfig(arr);
-                }
-              }
-            });
+            alert(
+              "To receive orders, fiat, crypto and payment fields must be filled! Please check data..."
+            );
           }
-          setNewFilterView(false);
-        } else {
-          alert(
-            "To receive orders, fiat, crypto and payment fields must be filled! Please check data..."
-          );
-        }
+        }, 1200);
       }}
     >
       <div className="h-full w-full rounded-10 p-[10px]">
