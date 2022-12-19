4af59de51608261dd7f7896c318a26dd427c63c8
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2022-12-19 14:20:43 +0600

    fix4

--- src/components/filterComponents/Refresh.jsx
+++ src/components/filterComponents/Refresh.jsx
@@ -86,7 +86,7 @@ const Refresh = ({
               "To receive orders, fiat, crypto and payment fields must be filled! Please check data..."
             );
           }
-        }, 2000);
+        }, 2500);
       }}
     >
       <div className="h-full w-full rounded-10 p-[10px]">
