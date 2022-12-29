06147120c1cfc8e44d6ccb9a55e204b346f47ed7
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2022-12-29 19:10:16 +0600

    total

--- src/components/orderComponents/Profit.jsx
+++ src/components/orderComponents/Profit.jsx
@@ -8,42 +8,40 @@ const Profit = () => {
 
   const [profit, setProfit] = useState(0);
 
-  // const getFee = () => {
-  //   let num = 0;
-  //   config.map((item) => {
-  //     if (item.currentFee !== null) {
-  //       num += Number(item.currentFee);
-  //       console.log(num, Number(item.currentFee));
-  //     }
-  //   });
-  //   console.log(num);
-  //   setProfit(num);
-  // };
-
-  // useEffect(() => {
-  //   if (config !== null && config.length >= 2) {
-  //     getFee();
-  //   }
-  // }, [config]);
   useEffect(() => {
-    function getTotalProfit() {
-      var arr = [];
-      config.map((item) => {
-        if (item.currentFee !== 0) {
-          arr.push(item.currentFee);
-        }
-      });
-
-      var sum = 0;
-      for (var i = 0; i < arr.length; i++) {
-        sum += arr[i];
+    var arr = [];
+    config.map((item) => {
+      if (item.currentFee !== 0) {
+        arr.push(item.currentFee);
       }
-
-      setProfit(sum.toFixed(4));
+    });
+    console.log(arr, "total");
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
     }
 
-    getTotalProfit();
+    console.log(sum, "totalSum");
   }, [config]);
+
+  // useEffect(() => {
+  //   function getTotalProfit() {
+  //     var arr = [];
+  //     config.map((item) => {
+  //       if (item.currentFee !== 0) {
+  //         arr.push(item.currentFee);
+  //       }
+  //     });
+
+  //     var sum = 0;
+  //     for (var i = 0; i < arr.length; i++) {
+  //       sum += arr[i];
+  //     }
+
+  //     setProfit(sum.toFixed(4));
+  //   }
+
+  // }, [config]);
 
   return (
     <div className="w-full h-[60px] bg-green rounded-15 mt-[30px] flex justify-between">
