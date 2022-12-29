002a8e8c320b7ff9c7ad8b413f15151d7fa9c525
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2022-12-30 01:49:39 +0600

    newEra

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -5,7 +5,8 @@ import Top from "./orderComponents/Top";
 import { feeFunction } from "../data/mainData";
 
 const Orders = () => {
-  const { orders } = useContext(StateContext);
+  const { orders, makerProcent, fiatRate, currentFiat } =
+    useContext(StateContext);
 
   const [currentIndex, setCurrentIndex] = useState(null);
   const [profitArray, setProfitArray] = useState([]);
@@ -33,137 +34,134 @@ const Orders = () => {
 
   return (
     <>
-      {ordersView === true ? (
+      {orders !== null && (
         <div className="2xl:w-[1290px] mx-auto">
           <Top />
 
-          {orders !== null && (
-            <>
-              {orders.map((item, index) => (
-                <div className="w-max h-max mb-[10px] rounded-20">
+          <>
+            {orders.map((item, index) => (
+              <div className="w-max h-max mb-[10px] rounded-20">
+                <div
+                  className={`2xl:w-[1070px] h-[88px] flex justify-between bg-white py-[12px] px-[30px] ${
+                    index === currentIndex ? "rounded-t-20" : " rounded-20"
+                  }`}
+                  key={index}
+                >
+                  {/* Mode */}
                   <div
-                    className={`2xl:w-[1070px] h-[88px] flex justify-between bg-white py-[12px] px-[30px] ${
-                      index === currentIndex ? "rounded-t-20" : " rounded-20"
-                    }`}
-                    key={index}
+                    className={`${
+                      item.type === 2 ? "bg-green" : "bg-orange"
+                    } w-max h-max rounded-2 my-auto px-[12px] py-[4px]`}
                   >
-                    {/* Mode */}
-                    <div
-                      className={`${
-                        item.type === 2 ? "bg-green" : "bg-orange"
-                      } w-max h-max rounded-2 my-auto px-[12px] py-[4px]`}
-                    >
-                      <p className="text-white font-normal text-12 leading-16">
-                        {item.type === 2 ? "Buy" : "Sell"}
-                      </p>
-                    </div>
-
-                    {/* Name */}
-                    <div className="grid min-w-[175px] h-max my-auto">
-                      <p className="text-14 leading-20 font-bold text-black mb-[6px]">
-                        {item.trade_user.name}
-                      </p>
-                      <div className="flex w-max h-max mx-auto my-auto text-12 leading-16 font-normal text-gray">
-                        <p>
-                          {`${item.trade_user.month_orders_count} orders  / ${
-                            Number(item.trade_user.month_finish_rate).toFixed(
-                              2
-                            ) * 100
-                          }% completion`}
-                        </p>
-                      </div>
-                    </div>
-
-                    {/* Price */}
-                    <div className="flex min-w-[100px] h-max my-auto">
-                      <p className="text-black text-18 font-bold leading-24">
-                        {item.price}
-                      </p>
-                      <p className="text-lightGray text-14 leading-20 font-bold w-max h-max mt-auto ml-[5px]">
-                        {item.fiat.name}
-                      </p>
-                    </div>
-
-                    {/* Available */}
-                    <div className="grid min-w-[195px] h-max my-auto">
-                      <div className="flex w-max h-max ">
-                        <p className="text-lightGray text-normal text-12 leading-16 my-auto">
-                          Available
-                        </p>
-                        <p className="text-14 leading-20 font-normal w-max h-max my-auto ml-[10px]">
-                          {`${item.amount} ${" "} ${item.asset.name}`}
-                        </p>
-                      </div>
-                      <div className="flex w-max h-max mt-[6px]">
-                        <p className="text-lightGray text-normal text-12 leading-16 my-auto">
-                          Limit
-                        </p>
-                        <p className="text-14 leading-20 font-normal w-max h-max my-auto ml-[10px]">
-                          {`${item.min_trans_amount} ${" "} -
-              ${" "} ${item.max_trans_amount} ${" "} ${item.fiat.name}`}
-                        </p>
-                      </div>
-                    </div>
-
-                    {/* Payment */}
-                    <div className="h-max my-auto flex w-[170px]">
-                      <div className="w-full h-max flex flex-wrap my-auto mx-auto">
-                        {item.trade_methods.map((obj, index) => (
-                          <div
-                            className={`border border-1 border-[${obj.color}] w-max h-max rounded-2 px-[5px] py-[4px] mx-[2px] my-[2px]`}
-                          >
-                            <p
-                              className={`text-[${obj.color}] text-12 leading-14 w-max font-normal`}
-                            >
-                              {obj.name}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-
-                    {/* Profit */}
-                    <div className="w-[88px] h-max flex my-auto">
-                      <p className="text-green text-18 leading-24 font-bold text-center">
-                        {feeFunction(
-                          makerProcent,
-                          Number(currentFiat.rates[0].rate),
-                          fiatRate,
-                          Number(item.price),
-                          setProfitArray
-                        )}
-                        %
-                      </p>
-                    </div>
-
-                    {/* Button */}
-                    <button
-                      type="button"
-                      className={`w-[50px] h-[50px]  border border-1  ${
-                        activeIndex === index
-                          ? "border-green bg-main"
-                          : "border-gray bg-white"
-                      } rounded-6 my-auto flex`}
-                      onClick={() => {
-                        setCurrentIndex(index);
-                        console.log(
-                          "кнопка для пеереброса профита в конфиг, пока disabled"
-                        );
-                      }}
-                    >
-                      <img
-                        src={plusOrders}
-                        alt="plus"
-                        className="w-[20px] h-[20px] m-auto "
-                      />
-                    </button>
+                    <p className="text-white font-normal text-12 leading-16">
+                      {item.type === 2 ? "Buy" : "Sell"}
+                    </p>
                   </div>
+
+                  {/* Name */}
+                  <div className="grid min-w-[175px] h-max my-auto">
+                    <p className="text-14 leading-20 font-bold text-black mb-[6px]">
+                      {item.trade_user.name}
+                    </p>
+                    <div className="flex w-max h-max mx-auto my-auto text-12 leading-16 font-normal text-gray">
+                      <p>
+                        {`${item.trade_user.month_orders_count} orders  / ${
+                          Number(item.trade_user.month_finish_rate).toFixed(2) *
+                          100
+                        }% completion`}
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Price */}
+                  <div className="flex min-w-[100px] h-max my-auto">
+                    <p className="text-black text-18 font-bold leading-24">
+                      {item.price}
+                    </p>
+                    <p className="text-lightGray text-14 leading-20 font-bold w-max h-max mt-auto ml-[5px]">
+                      {item.fiat.name}
+                    </p>
+                  </div>
+
+                  {/* Available */}
+                  <div className="grid min-w-[195px] h-max my-auto">
+                    <div className="flex w-max h-max ">
+                      <p className="text-lightGray text-normal text-12 leading-16 my-auto">
+                        Available
+                      </p>
+                      <p className="text-14 leading-20 font-normal w-max h-max my-auto ml-[10px]">
+                        {`${item.amount} ${" "} ${item.asset.name}`}
+                      </p>
+                    </div>
+                    <div className="flex w-max h-max mt-[6px]">
+                      <p className="text-lightGray text-normal text-12 leading-16 my-auto">
+                        Limit
+                      </p>
+                      <p className="text-14 leading-20 font-normal w-max h-max my-auto ml-[10px]">
+                        {`${item.min_trans_amount} ${" "} -
+              ${" "} ${item.max_trans_amount} ${" "} ${item.fiat.name}`}
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Payment */}
+                  <div className="h-max my-auto flex w-[170px]">
+                    <div className="w-full h-max flex flex-wrap my-auto mx-auto">
+                      {item.trade_methods.map((obj, index) => (
+                        <div
+                          className={`border border-1 border-[${obj.color}] w-max h-max rounded-2 px-[5px] py-[4px] mx-[2px] my-[2px]`}
+                        >
+                          <p
+                            className={`text-[${obj.color}] text-12 leading-14 w-max font-normal`}
+                          >
+                            {obj.name}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  {/* Profit */}
+                  <div className="w-[88px] h-max flex my-auto">
+                    <p className="text-green text-18 leading-24 font-bold text-center">
+                      {feeFunction(
+                        makerProcent,
+                        Number(currentFiat.rates[0].rate),
+                        fiatRate,
+                        Number(item.price),
+                        setProfitArray
+                      )}
+                      %
+                    </p>
+                  </div>
+
+                  {/* Button */}
+                  <button
+                    type="button"
+                    className={`w-[50px] h-[50px]  border border-1  ${
+                      currentIndex === index
+                        ? "border-green bg-main"
+                        : "border-gray bg-white"
+                    } rounded-6 my-auto flex`}
+                    onClick={() => {
+                      setCurrentIndex(index);
+                      console.log(
+                        "кнопка для пеереброса профита в конфиг, пока disabled"
+                      );
+                    }}
+                  >
+                    <img
+                      src={plusOrders}
+                      alt="plus"
+                      className="w-[20px] h-[20px] m-auto "
+                    />
+                  </button>
                 </div>
-              ))}
-            </>
-          )}
+              </div>
+            ))}
+          </>
         </div>
-      ) : null}
+      )}
     </>
   );
 };
