b89600fc7d327bb9b5bd73696927c1a8a003b6bb
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2023-01-04 15:20:43 +0600

    deleteChain

--- src/components/orderComponents/Chain.jsx
+++ src/components/orderComponents/Chain.jsx
@@ -5,17 +5,86 @@ import Profit from "./Profit";
 import { close, star, favouriteStar } from "../../images";
 
 const Chain = () => {
-  const { config, setConfig } = useContext(StateContext);
+  const {
+    config,
+    setConfig,
+    orders,
+    currentFee,
+    currentOrder,
+    currentPayment,
+    currentCrypto,
+    currentFiat,
+    globalId,
+    setGlobalId,
+    setCurrentOrder,
+    setCurrentFee,
+  } = useContext(StateContext);
 
   const [fav, setFav] = useState(false);
 
   const removeObject = (index) => {
     if (config.length === 1) {
+      console.log("clean config when only one object");
+      setGlobalId(globalId - 1);
+      setCurrentOrder(null);
+      setCurrentFee(null);
       setConfig([]);
     } else {
+      console.log("clean config when some onject");
       let arr = config;
       arr.splice(index, 1);
+      console.log(arr, "arr config remove click");
+      setCurrentOrder(null);
+      setCurrentFee(null);
+      setGlobalId(globalId - 1);
       setConfig(arr);
+    }
+
+    if (
+      currentFiat !== null &&
+      currentCrypto !== null &&
+      currentPayment !== null
+    ) {
+      const localObject = {
+        id: globalId,
+        mode: mode,
+        amount: amount,
+        defaultAmount: amount.length === 0 ? false : true,
+        fiat: currentFiat,
+        crypto: currentCrypto,
+        payments: currentPayment,
+        orders: orders,
+        currentOrder: currentOrder,
+        currentFee: currentFee,
+      };
+
+      if (config === null) {
+        const arr = [];
+        arr.push(localObject);
+
+        setConfig(arr);
+      } else {
+        let arr = config;
+
+        arr.map((item, index) => {
+          if (item.id === globalId && arr[arr.length - 1] !== globalId) {
+            if (JSON.stringify(item) !== JSON.stringify(localObject)) {
+              arr.splice(index, 1);
+              const insert = function (array, indexi, obj) {
+                return [...array.slice(0, indexi), obj, ...array.slice(indexi)];
+              };
+              arr = insert(arr, index, localObject);
+
+              setConfig(arr);
+            }
+          } else {
+            if (arr.length - 1 !== globalId) {
+              arr.push(localObject);
+              setConfig(arr);
+            }
+          }
+        });
+      }
     }
   };
 
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -19,7 +19,6 @@ const Main = () => {
     globalId,
     setConfig,
     currentOrder,
-    setOrdersView,
     currentFee,
   } = useContext(StateContext);
 
