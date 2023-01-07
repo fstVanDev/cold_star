ee2ca90ba5b4cf75412403faef6464b97fadd3f6
fstVanDev <104895006+fstVanDev@users.noreply.github.com>
2023-01-07 17:43:09 +0600

    asset

--- src/components/filterComponents/CryptoDropdown.jsx
+++ src/components/filterComponents/CryptoDropdown.jsx
@@ -19,25 +19,29 @@ const CryptoDrowdown = () => {
     }
   }, [crypto]);
 
+  // useEffect(() => {
+  //   if (currentCrypto === null) {
+  //     if (config !== null) {
+  //       if (config.length > 0) {
+  //         if (config[currentId].crypto !== null) {
+  //           setCurrentCrypto(config[currentId].crypto);
+  //           // setCryptoValue(config[currentId].crypto);
+  //           console.log("возврат crypto from config");
+  //         } else {
+  //           setCurrentCrypto(null);
+  //           console.log("дефолт");
+  //         }
+  //       }
+  //     } else {
+  //       setDefaultCrypto("Enter...");
+  //       setCryptoValue("");
+  //       setActiveCrypto(false);
+  //     }
+  //   }
+  // }, [currentCrypto]);
+
   useEffect(() => {
-    if (currentCrypto === null) {
-      if (config !== null) {
-        if (config.length > 0) {
-          if (config[currentId].crypto !== null) {
-            setCurrentCrypto(config[currentId].crypto);
-            // setCryptoValue(config[currentId].crypto);
-            console.log("возврат crypto from config");
-          } else {
-            setCurrentCrypto(null);
-            console.log("дефолт");
-          }
-        }
-      } else {
-        setDefaultCrypto("Enter...");
-        setCryptoValue("");
-        setActiveCrypto(false);
-      }
-    }
+    console.log(currentCrypto);
   }, [currentCrypto]);
 
   return (
